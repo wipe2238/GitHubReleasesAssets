8c4444558add705c7fc8a4d001ea4ccdef773423
Wipe <wipe.git@gmail.com>
2024-06-12 12:06:25 +0200

    added stub of http response status handling

--- cmd/GitHubReleasesAssets/json.go
+++ cmd/GitHubReleasesAssets/json.go
@@ -47,9 +47,18 @@ func GetJSON(Url string) *fastjson.Value {
 		os.Exit(1)
 	}
 
-	if Response.StatusCode != http.StatusOK {
-		fmt.Printf("Status:%d\n", Response.StatusCode)
+	switch Response.StatusCode {
+	case http.StatusOK:
+		break
+	case http.StatusNotFound:
+		fmt.Print("Repository not found")
 		os.Exit(1)
+	case http.StatusTeapot:
+		fmt.Print("Repository is a teapot")
+		os.Exit(1)
+	default:
+		fmt.Printf("HTTP status %d %s\n", Response.StatusCode, http.StatusText(Response.StatusCode))
+		os.Exit(1) // oof
 	}
 
 	var Result *fastjson.Value
